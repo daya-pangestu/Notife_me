cdc62b15decbd8ca34f101c046cbbae25fbd9e0a
dayfrk <day.pangestu@gmail.com>
2019-11-20 04:32:14 +0700

    Task 3: Add a notification action button

--- app/src/main/java/com/daya/notifyme/MainActivity.kt
+++ app/src/main/java/com/daya/notifyme/MainActivity.kt
@@ -3,8 +3,10 @@
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -12,16 +14,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
  class MainActivity : AppCompatActivity() {
 
      companion object {
          const val PRIMARY_CHANNEL_ID = "primary_channel_id"
          const val NOTIFICATION_ID = 0
+
+         const val ACTION_UPDATE_NOTIFICATION =
+             "com.daya.notifyme.ACTION_UPDATE_NOTIFICATION"
      }
 
      lateinit var mNotifyManager: NotificationManager
-
+      var mReceiver =NotificationReceiver()
 
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
@@ -43,10 +53,20 @@ import kotlinx.android.synthetic.main.activity_main.*
          button_cancel.setOnClickListener {
              cancelNotification()
          }
+
+         registerReceiver(mReceiver, IntentFilter(ACTION_UPDATE_NOTIFICATION))
+     }
+
+     override fun onDestroy() {
+         unregisterReceiver(mReceiver)
+         super.onDestroy()
      }
 
 
      fun sendNotification() {
+         val updateIntent = Intent(ACTION_UPDATE_NOTIFICATION)
+         val updatePendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID,updateIntent,PendingIntent.FLAG_ONE_SHOT)
+
          mNotifyManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
          if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -64,6 +84,8 @@ import kotlinx.android.synthetic.main.activity_main.*
              }
              mNotifyManager.createNotificationChannel(notificationChannel)
              val notifyBuilder = getNotificationBuilder()
+             notifyBuilder.addAction(R.drawable.ic_update ,"Update Notification",updatePendingIntent)
+
              mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build())
          }
          setNotificationButtonState(
@@ -71,7 +93,6 @@ import kotlinx.android.synthetic.main.activity_main.*
              isUpdateEnabled = true,
              isCancelEnabled = true
          )
-
      }
 
 
@@ -136,24 +157,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
      }
 
+     inner class NotificationReceiver : BroadcastReceiver(){
+         override fun onReceive(context: Context?, intent: Intent?) {
+             updateNotification()
+         }
+     }
+
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
